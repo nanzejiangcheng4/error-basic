dd219c08633590e299222b158aaac2f6c5c9d848
nanzejiangcheng4 <nanzejiangcheng4@gmail.com>
2025-06-15 21:33:11 +0900

    NuxtErrorBoundaryの基礎的なところを実装

--- pages/errorHandlerBasic.vue
+++ pages/errorHandlerBasic.vue
@@ -1,4 +1,10 @@
 <template>
-  <ErrorGeneratorBasic />
-  <p><NuxtLink v-bind:to="{ name: 'index' }" />戻る</p>
+  <NuxtErrorBoundary>
+    <ErrorGeneratorBasic />
+    <template v-slot:error="errorProps">
+      <p>エラーが発生しました！</p>
+      <p>{{ errorProps.error }}</p>
+    </template>
+  </NuxtErrorBoundary>
+  <p><NuxtLink v-bind:to="{ name: 'index' }">戻る</NuxtLink></p>
 </template>
