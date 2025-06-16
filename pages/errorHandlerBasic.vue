994cc2bbc741f12e2f3b48b5eafb82a200c45458
nanzejiangcheng4 <nanzejiangcheng4@gmail.com>
2025-06-16 10:22:27 +0900

    nuxtを3.6.2へダウングレードして、取り敢えず実装。あとで最新版で上手くいくように修正すること

--- pages/errorHandlerBasic.vue
+++ pages/errorHandlerBasic.vue
@@ -1,10 +1,22 @@
+<script setup lang="ts">
+const onResetButtonClick = (error: Ref) => {
+  console.log(error);
+  error.value = null;
+};
+</script>
+
 <template>
   <NuxtErrorBoundary>
     <ErrorGeneratorBasic />
     <template v-slot:error="errorProps">
-      <p>エラーが発生しました！</p>
+      <p>エラーが発生しました!</p>
       <p>{{ errorProps.error }}</p>
+      <button v-on:click="onResetButtonClick(errorProps.error)">
+        エラーを解消
+      </button>
     </template>
   </NuxtErrorBoundary>
-  <p><NuxtLink v-bind:to="{ name: 'index' }">戻る</NuxtLink></p>
+  <p>
+    <NuxtLink v-bind:to="{ name: 'index' }"> 戻る </NuxtLink>
+  </p>
 </template>
